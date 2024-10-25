d667d54b915e2106f26f3888eac219c3bbf5301e
Nimrat Gill <nimratgill@Nimrats-MBP.lan>
2024-10-25 13:48:12 -0700

    add active state to nav

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Header from './components/Header/Header.jsx';
 import LoginPage from './pages/LoginPage/LoginPage.jsx';
 import RecordPage from './pages/RecordPage/RecordPage.jsx';
 import AnalysisPage from './pages/AnalysisPage/AnalysisPage.jsx';
+import TutorialPage from './pages/TutorialPage/TutorialPage.jsx';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage.jsx';
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/record" element={<RecordPage/>}/>
         <Route path="/analyze" element={<AnalysisPage/>}/>
+        <Route path="/tutorial" element={<TutorialPage/>}/>
+        <Route path="*" element={<NotFoundPage/>}/>
       </Routes>
     </BrowserRouter>
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,41 @@
 // src/components/Header.jsx
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Header.scss';
 
 function Header() {
   return (
     <nav className="navbar">
-        <Link to = "/" className="navbar__logo">KinetiScan</Link>
-        <ul className="navbar__links">
-            <li><a href='/#about'>About</a></li>
-            <li><Link to = "/login">Login</Link></li>
-            <li><a href="/tutorial">Tutorial</a></li>
-        </ul>
+      <NavLink to="/" className="navbar__logo">
+        KinetiScan
+      </NavLink>
+      <ul className="navbar__links">
+        <li>
+          <a href="/#about">About</a>
+        </li>
+        <li>
+          <NavLink
+            to="/login"
+            className={({ isActive }) => 
+              isActive ? "navbar__link navbar__link--active" : "navbar__link"
+            }
+          >
+            Login
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/tutorial"
+            className={({ isActive }) => 
+              isActive ? "navbar__link navbar__link--active" : "navbar__link"
+            }
+          >
+            Tutorial
+          </NavLink>
+        </li>
+      </ul>
     </nav>
   );
-};
+}
 
 export default Header;
