16bf5ae04704037847a4dd8920979a613865f5ef
Nimrat Gill <nimratgill@Nimrats-MBP.lan>
2024-10-27 13:12:30 -0700

    finish responsiveness yayy

--- src/pages/TutorialPage/TutorialPage.jsx
+++ src/pages/TutorialPage/TutorialPage.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import './TutorialPage.scss'
+import React from 'react';
+import './TutorialPage.scss';
 
 function TutorialPage() {
   return (
     <div>
       <h1 className="title">How To Use</h1>
-      <iframe src="https://scribehow.com/embed/KinetiScan_How__To__GSOb-n64S7itiBO_FVhSnQ?as=scrollable" width="100%" height="640" allowFullScreen frameBorder="0"></iframe>
+      <div className="iframe-container">
+        <iframe
+          src="https://scribehow.com/embed/KinetiScan_How__To__GSOb-n64S7itiBO_FVhSnQ?as=scrollable"
+          allowFullScreen
+          frameBorder="0"
+        ></iframe>
+      </div>
     </div>
-  )
+  );
 }
 
-export default TutorialPage
+export default TutorialPage;
