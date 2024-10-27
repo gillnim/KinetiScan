26d2df65bd46804fd36d769374cfe0460cda21b7
Nimrat Gill <nimratgill@Nimrats-MBP.lan>
2024-10-26 17:42:15 -0700

    fix footer positioning

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// src/App.jsx
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Homepage from './pages/HomePage/HomePage.jsx';
@@ -9,23 +10,27 @@ import TutorialPage from './pages/TutorialPage/TutorialPage.jsx';
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage.jsx';
 import Footer from './components/Footer/Footer.jsx';
 import ConnectPage from './pages/ConnectPage/ConnectPage.jsx';
+import './App.scss'; // Import the stylesheet here
 
 function App() {
   return (
     <BrowserRouter>
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Homepage/>}/>
-        <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/record" element={<RecordPage/>}/>
-        <Route path="/analyze" element={<AnalysisPage/>}/>
-        <Route path="/tutorial" element={<TutorialPage/>}/>
-        <Route path="/contact" element={<ConnectPage/>}/>
-        <Route path="*" element={<NotFoundPage/>}/>
-      </Routes>
-      <Footer/>
+      <div className="app-container">
+        <Header />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/record" element={<RecordPage />} />
+            <Route path="/analyze" element={<AnalysisPage />} />
+            <Route path="/tutorial" element={<TutorialPage />} />
+            <Route path="/contact" element={<ConnectPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </BrowserRouter>
-
   );
 }
 
